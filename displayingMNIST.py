52f3a9c418ef8c9c01d8f81cadacf238bf8d0508
bblacklock <bblacklock25@gmail.com>
2022-03-27 00:16:44 -0500

    updatesToDisplayingMNIST

--- displayingMNIST.py
+++ displayingMNIST.py
@@ -1,6 +1,7 @@
 import cv2 as cv
 import numpy as np
 import idx2numpy
+import matplotlib.pyplot as plt
 
 # Load Train Images and Labels Files
 imageTrainFile = 'MNIST/DataUncompressed/train-images.idx3-ubyte'
@@ -10,10 +11,19 @@ labelTrainFile = 'MNIST/DataUncompressed/train-labels.idx1-ubyte'
 imageTrainArr = idx2numpy.convert_from_file(imageTrainFile)
 labelTrainArr = idx2numpy.convert_from_file(labelTrainFile)
 
+fig = plt.figure(figsize=(12, 9))
+
+for i in range(30):
+    ax = fig.add_subplot(5, 6, i+1)
+    ax.imshow(imageTrainArr[i], cmap=plt.get_cmap('gray'))
+    ax.set_title('Label: {y}' .format(y=labelTrainArr[i]))
+    plt.axis('off')
+plt.show()
+
 # Display using opencv
-for i in range(0, imageTrainArr.size):
-    cv.imshow(str(labelTrainArr[i]), imageTrainArr[i])
-    cv.waitKey()
-    cv.destroyAllWindows
+# for i in range(0, imageTrainArr.size):
+#     cv.imshow(str(labelTrainArr[i]), imageTrainArr[i])
+#     cv.waitKey()
+#     cv.destroyAllWindows
 
 
