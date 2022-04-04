873baeb76d26b9b351d7ee6b708c943842bd0e28
bblacklock <bblacklock25@gmail.com>
2022-04-03 22:01:11 -0500

    updates

--- logisticReg-Brendan.py
+++ logisticReg-Brendan.py
@@ -47,34 +47,36 @@ def normalizeAndGenerateDataMatrix(imageArr):
     return np.insert(imageArr, imageArr.shape[1], 1, axis=1)  # Insert Column of 1's at end of each image and Return Matrix
 
 # Gradient descent with momentum function to fit model
-def gradientDescent(DataX, DataT, LabelArr):
-    start_time = time.time()  # Define start time
-    M = DataX.shape[1]        # Define number of features (M) + 1 (785)
-    K = DataT.shape[1]        # Define number of classes (K) (10)
-    Beta = 0.9                # Define beta of 0.9
-    prevgradient = 0          # Initialize previous gradient to 0
-    W = np.random.rand((M),K) # Initialize weight matrix of size (785x10) to random values
-    maxEpochs = 1000          # Define max epochs (iterations) to 1000 
-    NE = 0                    # Initialize number of iterations to 0
-    LR = .01                  # Define learning rate of 0.1
+def gradientDescent(DataX, DataT):
+    start_time = time.time()    # Define start time
+    M = DataX.shape[1]          # Define number of features (M) + 1 (785)
+    K = DataT.shape[1]          # Define number of classes (K) (10)
+    Beta = 0.9                  # Define beta of 0.9
+    prevgradient = 0            # Initialize previous gradient to 0
+    W = np.random.rand((M),K)   # Initialize weight matrix of size (785x10) to random values
+    maxEpochs = 1000            # Define max epochs (iterations) to 1000 
+    NE = 0                      # Initialize number of iterations to 0
+    LR = .01                    # Define learning rate of 0.1
     for i in range(maxEpochs):  # For loop to iterate through epochs (iterations)
         temp1 = sp.softmax(np.dot(DataX, W) - np.max(np.dot(DataX, W)), axis=1)             # Define temp1 as softmax mapping of computed probalities with current weight matrix values
         gradient = np.dot(np.transpose(DataX), temp1) - np.dot(np.transpose(DataX), DataT)  # Calculate gradient
         gradient = Beta * prevgradient + (1.0 - Beta) * gradient                            # Calculate gradient with momentum 
+        prevW = W
         W = W - LR * gradient                                                               # Calculate new weight matrix
         prevgradient = gradient                                                             # Assign previous gradient the value of current gradient
         NE += 1                                                                             # Increment number of iterations by 1
         loss = calculateLoss(W, DataX, DataT)                                               # Calculate loss
         accuracyPercentage = accuracy(W, DataX, DataT)                                      # Calculate accuracy
-        print("Epoch", NE,"- Accuracy (%) {0:.2f}" .format(accuracyPercentage), ", Loss {0:.2f}" .format(loss))  # Print Epochs, Accuracy, and Loss
-        
+        # print("Epoch", NE,"- Accuracy (%) {0:.2f}" .format(accuracyPercentage), ", Loss {0:.2f}" .format(loss))  # Print Epochs, Accuracy, and Loss
+        if (np.linalg.norm(W - prevW) < 1e-3):
+            break
     total_time = time.time() - start_time                                                   # Define total time as current time - start time
     return W, NE, total_time                                                                # Return weight matrix, number of iterations, and total time taken
 
 K = 10                                # Number of Classes
 N = imageTrainArrLinearized.shape[0]  # Number of Images
 M = imageTrainArrLinearized.shape[1]  # Number of Features
-T = generateT(N, K, labelTrainArr)    # Generate T Matrix from labels (one-hot encoding)
+TTrain = generateT(N, K, labelTrainArr)    # Generate T Matrix from labels (one-hot encoding)
 imageDataMatrixTrain = normalizeAndGenerateDataMatrix(imageTrainArrLinearized)              # Normalize and generate data matrix from images array
-trainedModel, numIter, totalTime = gradientDescent(imageDataMatrixTrain, T, labelTrainArr)  # Fit model 
+trainedModel, numIter, totalTime = gradientDescent(imageDataMatrixTrain, TTrain)  # Fit model 
 print("Number of Iterations", numIter, "Total Time", totalTime)
