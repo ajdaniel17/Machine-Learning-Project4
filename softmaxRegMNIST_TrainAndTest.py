7e7d618b748d10880b750fb75583aa25aee4275e
bblacklock <bblacklock25@gmail.com>
2022-04-05 12:00:51 -0500

    formattedMNIST

--- softmaxRegMNIST_TrainAndTest.py
+++ softmaxRegMNIST_TrainAndTest.py
@@ -6,11 +6,16 @@ import scipy.special as sp
 
 imageTrainFile = 'MNIST/DataUncompressed/train-images.idx3-ubyte'  # Load Train Images File
 labelTrainFile = 'MNIST/DataUncompressed/train-labels.idx1-ubyte'  # Load Train Labels File
+imageTestFile = 'MNIST/DataUncompressed/test-images.idx3-ubyte'  # Load Test Images File
+labelTestFile = 'MNIST/DataUncompressed/test-labels.idx1-ubyte'  # Load Test Labels File
 
 imageTrainArr = idx2numpy.convert_from_file(imageTrainFile)  # Convert training images to numpy arrays
 labelTrainArr = idx2numpy.convert_from_file(labelTrainFile)  # Convert training labels to numpy arrays
+imageTestArr = idx2numpy.convert_from_file(imageTestFile)  # Convert testing images to numpy arrays
+labelTestArr = idx2numpy.convert_from_file(labelTestFile)  # Convert testing labels to numpy arrays
 
-imageTrainArrLinearized = imageTrainArr.reshape(imageTrainArr.shape[0], imageTrainArr.shape[1] * imageTrainArr.shape[2])  # Linearize images
+imageTrainArrLinearized = imageTrainArr.reshape(imageTrainArr.shape[0], imageTrainArr.shape[1] * imageTrainArr.shape[2])  # Linearize training images
+imageTestArrLinearized = imageTestArr.reshape(imageTestArr.shape[0], imageTestArr.shape[1] * imageTestArr.shape[2])  # Linearize testing images
 
 # Calculate loss of model
 def calculateLoss(W, DataX, DataT):
@@ -25,7 +30,7 @@ def calculateLoss(W, DataX, DataT):
     return totalLoss                                        # Return total loss
 
 # Calculate accuracy of model
-def accuracy(W, DataX, DataT):
+def calculateAccuracy(W, DataX, DataT):
     SIZE = DataX.shape[0]      # Define SIZE from number of rows of DataX (Number of Images) 
     yHat = np.dot(DataX, W)    # Compute probabilities for each class
     errors = 0                 # Initialize errors to 0
@@ -61,26 +66,32 @@ def gradientDescent(DataX, DataT):
         temp1 = sp.softmax(np.dot(DataX, W) - np.max(np.dot(DataX, W)), axis=1)             # Define temp1 as softmax mapping of computed probalities with current weight matrix values
         gradient = np.dot(np.transpose(DataX), temp1) - np.dot(np.transpose(DataX), DataT)  # Calculate gradient
         gradient = Beta * prevgradient + (1.0 - Beta) * gradient                            # Calculate gradient with momentum 
-        prevW = W
+        prevW = W                                                                           # Assign prevW value of current W
         W = W - LR * gradient                                                               # Calculate new weight matrix
         prevgradient = gradient                                                             # Assign previous gradient the value of current gradient
         NE += 1                                                                             # Increment number of iterations by 1
-        # loss = calculateLoss(W, DataX, DataT)                                               # Calculate loss
-        # accuracyPercentage = accuracy(W, DataX, DataT)                                      # Calculate accuracy
-        # print("Epoch", NE,"- Accuracy (%) {0:.2f}" .format(accuracyPercentage), ", Loss {0:.2f}" .format(loss))  # Print Epochs, Accuracy, and Loss
-        # print(np.linalg.norm(np.mean(W, axis=0) - np.mean(prevW, axis=0)))
-        if (np.linalg.norm(np.mean(W, axis=0) - np.mean(prevW, axis=0)) < 8e-3):
-            print(accuracy(W, DataX, DataT))
-            break
+        if (np.linalg.norm(np.mean(W, axis=0) - np.mean(prevW, axis=0)) < 8e-3):            # If weight vector is not changing,
+            break                                                                           # break loop
     total_time = time.time() - start_time                                                   # Define total time as current time - start time
     return W, NE, total_time                                                                # Return weight matrix, number of iterations, and total time taken
 
-K = 10                                   # Number of Classes
-N = imageTrainArrLinearized.shape[0]     # Number of Images
-M = imageTrainArrLinearized.shape[1]     # Number of Features
-TTrain = generateT(N, K, labelTrainArr)  # Generate T Matrix from labels (one-hot encoding)
-imageDataMatrixTrain = normalizeAndGenerateDataMatrix(imageTrainArrLinearized)    # Normalize and generate data matrix from images array
+K = 10                                        # Number of Classes
+NTrain = imageTrainArrLinearized.shape[0]     # Number of Training Images
+NTest = imageTestArrLinearized.shape[0]       # Number of Testing Images
+M = imageTrainArrLinearized.shape[1]          # Number of Features
+TTrain = generateT(NTrain, K, labelTrainArr)  # Generate TTrain Matrix from training labels (one-hot encoding)
+TTest = generateT(NTest, K, labelTestArr)     # Generate TTest Matrix from testing labels (one-hot encoding)
+imageDataMatrixTrain = normalizeAndGenerateDataMatrix(imageTrainArrLinearized)    # Normalize and generate training data matrix from images array
+imageDataMatrixTest = normalizeAndGenerateDataMatrix(imageTestArrLinearized)      # Normalize and generate test data matrix from images array
 trainedModel, numIter, totalTime = gradientDescent(imageDataMatrixTrain, TTrain)  # Fit model 
-print("Number of Iterations", numIter, "Total Time", totalTime)
-np.savez_compressed('trainedModelMNIST', x=trainedModel)
-print(np.array_equal)
+print("Number of Iterations - ", numIter, ", Total Time (s)", totalTime)          # Print number of iterations and total time taken
+# np.savez_compressed('trainedModelMNIST', x=trainedModel)                        # Save trained model
+trainLoss = calculateLoss(trainedModel, imageDataMatrixTrain, TTrain)             # Calculate training loss
+trainAccuracy = calculateAccuracy(trainedModel, imageDataMatrixTrain, TTrain)     # Calculate training accuracy
+print('Training - Loss =', trainLoss, ", Accuracy (%) = ", trainAccuracy)         # Print training loss and accuracy
+testLoss = calculateLoss(trainedModel, imageDataMatrixTest, TTest)                # Calculate testing loss
+testAccuracy = calculateAccuracy(trainedModel, imageDataMatrixTest, TTest)        # Calculate testing accuracy
+print('Testing - Loss =', testLoss, ", Accuracy (%) = ", testAccuracy)            # Print testing loss and accuracy
+
+
+
